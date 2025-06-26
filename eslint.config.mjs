de5077a78e0ff8be1206754b02b33f36b5632a92
fazlerabbi125 <fazle.rabbi125faiyaz@gmail.com>
2025-06-26 23:57:33 +0600

    chore: Initial project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+		files: ["**/*.ts", "**/*.tsx"],
+    // ignores: [],
+		rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+		},
+	},
 ];
 
 export default eslintConfig;
